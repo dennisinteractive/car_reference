5473ec60ca9d66e12e303ee66355fce98a0b9f2f
Cornel Andreev <hugronaphor@gmail.com>
2015-07-24 17:52:50 +0100

    #23986: Convert to OOP the JS part and notify user when the field is locked.

--- car_reference.admin.js
+++ car_reference.admin.js
@@ -1,49 +1,96 @@
-(function($) {
+(function ($) {
 
-  Drupal.behaviors.carReferenceFieldAdminSelect = {
-    attach: function(context) {
-
-      $('.ajax-vocabulary-conf-vocabulary select').each(function(index) {
-        appendVocabularyOptions($(this));
-      });
-
-      $('.ajax-vocabulary-conf-vocabulary select', context).bind('change', function(context) {
-        appendVocabularyOptions($(this));
-      });
+    Drupal.behaviors.carReferenceFieldAdminSelect = {
+        attach: function (context) {
 
 
-      // Replace the options depending on chosen value in parent select.
-      function appendVocabularyOptions(selectEl) {
-        var vocab = selectEl.val(),
-                parentWrapper = selectEl.closest('.fieldset-wrapper'),
-                url = '/car_reference/vocabulary_fields/' + vocab;
-
-        $.get(url, function(data) {
-          var current_select = parentWrapper.find('.ajax-vocabulary-conf-ref-field-name select'),
-                  selectedOption = current_select.find("option:selected").attr('value');
-
-          // Clean-Up current options.
-          current_select.empty();
-
-          // Loop returned json options and append them as options.
-          $.each(data, function(key, value) {
-            if (selectedOption == key) {
-              current_select.append($("<option></option>")
-                      .attr('value', key)
-                      .attr('selected', 'selected')
-                      .text(value));
-            }
-            else {
-              current_select.append($("<option></option>")
-                      .attr('value', key)
-                      .text(value));
+            var dSettings = Drupal.settings;
+            var x = false;
+            if (dSettings.car_reference != undefined) {
+                x = (dSettings.car_reference != undefined) ? dSettings.car_reference.hasData : false;
             }
 
-          });
-        });
-      };
+            // Replace the options depending on chosen value in parent select.
+            var detectVocabularyOptions = {
+                ajaxBaseUrl: '/car_reference/vocabulary_fields/',
+                noWayToEdit: Drupal.settings.car_reference.noWayToEdit.length > 0,
 
-    }
-  };
+                detectOptions: function () {
+
+                    this.currentSelect = this.parentWrapper.find('.ajax-vocabulary-conf-ref-field-name select');
+                    this.selectedOption = this.currentSelect.find("option:selected").attr('value');
+                    // Clean-Up current options.
+                    this.currentSelect.empty();
+
+                    var currSelectEl = this.currentSelect;
+
+                    // This means that this field configuration can't be edited anymore due to populated records in the
+                    // current table. This means that all the configuration needs to be done programmatically.
+                    if (this.noWayToEdit) {
+                        var customOptions = {
+                            'none': Drupal.t('Error: The field is locked and not all field settings are set.'),
+                        };
+
+                        // Append the options.
+                        this.appendOptions(customOptions, currSelectEl);
+                        return;
+                    }
+
+
+                    var tempThis = this;
+                    $.get(this.ajaxUrl, function (data) {
+                        // Append the options.
+                        tempThis.appendOptions(data, currSelectEl);
+                    });
+                },
+
+                // Loop returned JSON options and append those as options.
+                appendOptions: function (data, currSelectEl) {
+                    var tempThis = this;
+                    $.each(data, function (key, value) {
+
+                        if (tempThis.selectedOption == key) {
+                            currSelectEl.append($("<option></option>")
+                                .attr('value', key)
+                                .attr('selected', 'selected')
+                                .text(value));
+                        }
+                        else {
+                            currSelectEl.append($("<option></option>")
+                                .attr('value', key)
+                                .text(value));
+                        }
+
+                    });
+                },
+
+                // detectVocabularyOptions initialization function.
+                // @selectEl - select html element object.
+                init: function (selectEl) {
+                    // Initiate needed variables.
+                    this.vocab = selectEl.val();
+                    this.parentWrapper = selectEl.closest('.fieldset-wrapper');
+                    this.ajaxUrl = this.ajaxBaseUrl + this.vocab;
+                    // Process the select element if all the data is available.
+                    if (this.vocab.length && this.parentWrapper.length) {
+                        this.detectOptions();
+                    }
+                }
+            }
+
+            // Initial call.
+            $('.ajax-vocabulary-conf-vocabulary select').each(function (index) {
+                // Skip the make, as it doesn't make sense.
+                if (index !== 0) {
+                    detectVocabularyOptions.init($(this));
+                }
+            });
+
+            // On Change call.
+            $('.ajax-vocabulary-conf-vocabulary select', context).bind('change', function (context) {
+                detectVocabularyOptions.init($(this));
+            });
+        }
+    };
 
 })(jQuery);
